8af48241ed923270b77c61e0b7c1ad4db8b7ead5
shark <spacedicknew@gmail.com>
2023-04-16 09:44:30 -0700

    lesson4

---
+++ lesson_3/client.py
@@ -0,0 +1,42 @@
+import argparse
+import socket
+
+BLOCK_LEN: int = 1024
+EOM: bytes = b'ENDOFMESSAGE___'
+WELCOME_MESSAGE: str = 'Hi! What is your name?'
+
+
+def parse_cli_arguments():
+    parser = argparse.ArgumentParser(description="Эхо сервер")
+    parser.add_argument('--host', type=str, default='localhost')
+    parser.add_argument('--port', type=int, default=9999)
+    return parser.parse_args()
+
+
+def read_message(connection) -> bytes:
+    message = b''
+    while len(message) < len(EOM) or message[-len(EOM):] != EOM:
+        data = connection.recv(BLOCK_LEN)
+        if not data:
+            print('Ошибка отправки данных. Разрыв соединения с сервером')
+            break
+        message += data
+        return message
+
+
+def main(host, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientsocket:
+        clientsocket.connect((host, port))
+        print(WELCOME_MESSAGE)
+
+        while True:
+            message = input('>').encode('utf-8')
+            message += EOM
+            clientsocket.send(message)
+            response = read_message(clientsocket).decode()
+            print(response[:-len(EOM)])
+
+
+if __name__ == '__main__':
+    args = parse_cli_arguments()
+    main(host=args.host, port=args.port)
