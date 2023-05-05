b342eee533ce8c434332fbe1552ed890e7c74bab
shark <spacedicknew@gmail.com>
2023-05-05 10:34:10 -0700

    9

---
+++ 9/client.py
@@ -0,0 +1,79 @@
+import argparse
+import json
+import subprocess
+import time
+from socket import socket, AF_INET, SOCK_STREAM
+import turtle
+
+
+class Client:
+
+    def __init__(self, host: str, port: int, client_type: str = 'write'):
+        self._BLOCK_LEN: int = 1024
+        self._EOM: bytes = b'ENDOFMESSAGE___'
+        self._host: str = host
+        self._port: int = port
+        self._client_type: bool = False if client_type == 'read' else True
+        self.user_name: str = ''
+
+    @staticmethod
+    def parse_cli_arguments() -> turtle(str, int):
+        parser = argparse.ArgumentParser(description='Эхо-клиент')
+        parser.add_argument('--host', type=str, default='localhost')
+        parser.add_argument('--port', type=int, default=9999)
+        parser.add_argument('--type', type=str, default=True)
+        return parser.parse_args()
+
+    def get_client_name(self) -> None:
+        if self._client_type:
+            print('Введите Ваш ник_нейм')
+            self.user_name = input('Ник? >: ')
+            print()
+
+    def start_chat_process(self) -> None:
+        if self._client_type:
+            subprocess.Popen('python3 client.py --type=read', shell=True)
+
+    def get_message(self, get_socket: socket) -> None:
+        server_message = get_socket.recv(self._BLOCK_LEN)
+        messages = self.parse_server_messages(server_message)
+        self.printing_messages(messages)
+
+    def send_message(self, send_socket: socket, message: str) -> None:
+        client_data = {'user_name': self.user_name, 'message': message}
+        encoded_data = json.dumps(client_data).encode('utf-8')
+        send_socket.send(encoded_data + self._EOM)
+
+    def parse_server_messages(self, server_message: bytes) -> list:
+        messages = server_message.split(self._EOM)[:-1]
+        return messages
+
+    def printing_messages(self, messages: list) -> None:
+        for message in messages:
+            message = json.loads(message.decode('utf-8'))
+            print(f"{message['user_name']}: {message['message']}")
+
+    def start(self) -> None:
+
+        with socket(AF_INET, SOCK_STREAM) as s:
+            s.connect((self._host, self._port))
+
+            self.get_client_name()
+            self.start_chat_process()
+
+            while True:
+                if self._client_type:
+                    message = input('>: ')
+                    if message == '/exit':
+                        break
+
+                    self.send_message(s, message)
+                    continue
+
+                self.get_message(s)
+
+
+if __name__ == '__main__':
+    args = Client.parse_cli_arguments()
+    client = Client(host=args.host, port=args.port, client_type=args.type)
+    client.start()
---
+++ 9/server.py
@@ -0,0 +1,81 @@
+import argparse
+from select import select
+from socket import socket, AF_INET, SOCK_STREAM
+import turtle
+
+
+class Server:
+
+    def __init__(self, host, port):
+        self._BLOCK_LEN: int = 1024
+        self._host: str = host
+        self._port: int = port
+        self._connections: list = []
+
+
+    @staticmethod
+    def parse_server_arguments() -> turtle(str, int):
+        arguments = argparse.ArgumentParser(description='Server host ip-address and port')
+        arguments.add_argument('--host', type=str, default='localhost')
+        arguments.add_argument('--port', type=int, default=9999)
+        return arguments.parse_args()
+
+    def read_requests(self, read_for: list) -> dict:
+        responses = {}
+
+        for read_socket in read_for:
+            try:
+                request = read_socket.recv(self._BLOCK_LEN)
+                responses[read_socket] = request
+            except ConnectionResetError:
+                print(f'Client is shutdown {read_socket}')
+                self._connections.remove(read_socket)
+        return responses
+
+    def write_response(self, write_list: list, requests: dict):
+        for write_socket in write_list:
+            if write_socket in requests:
+                try:
+                    response = requests[write_socket]
+                    for connection in self._connections:
+                        connection.send(response)
+                except BrokenPipeError:
+                    print(f'Client is shutdown {write_socket}')
+                    write_socket.close()
+                    self._connections.remove(write_socket)
+
+    def start(self):
+
+        with socket(AF_INET, SOCK_STREAM) as server_socket:
+            server_socket.bind((self._host, self._port))
+            server_socket.listen(5)
+            server_socket.settimeout(0.2)
+            print('Server is started!')
+
+            while True:
+
+                try:
+                    connection, address = server_socket.accept()
+                except OSError:
+                    pass
+                else:
+                    print(f'Connection accepted from {address}')
+                    self._connections.append(connection)
+                finally:
+
+                    timeout = 10
+
+                    try:
+                        read_list, write_list, errors = select(self._connections, self._connections, [], timeout)
+                    except OSError:
+                        pass
+
+                    requests = self.read_requests(read_list)
+                    if requests:
+                        self.write_response(write_list, requests)
+
+
+if __name__ == '__main__':
+    args = Server.parse_server_arguments()
+    server = Server(args.host, args.port)
+    server.start()
